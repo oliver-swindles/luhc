6579a8e9699cf641648afaa128efeaf1f4ee9fba
Oliver Swindles <86194326+oliver-swindles@users.noreply.github.com>
2025-05-27 13:57:22 +0100

    Added schema markup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,17 +9,16 @@
 	let { children } = $props();
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-  import LUHCLogo from "$lib/images/luhc-logo.png";
 	onMount(() => {
     AOS.init({ duration: 800, easing: 'ease-in-out', once: true });
   });
 </script>
 
 <svelte:head>
-	<title>Lancaster University Hockey Club</title>
+	<title>Lancaster University Hockey Club | LUHC</title>
   <link rel="apple-touch-icon" href="/favicon.png">
 	<link rel="preconnect" href="https://fonts.gstatic.com" />
-	<meta name="description" content="The official home page of Lancaster university hockey club. All are welcome.">
+	<meta name="description" content="Official site of Lancaster university hockey club (LUHC). Find team info, match updates, events, and how to get involved. #RosesAreRed">
 	<meta property="og:title" content="Lancaster University Hockey Club"/>
   <meta name="twitter:title" content="Lancaster University Hockey Club"/>
 
@@ -27,14 +26,59 @@
 
   <meta property="og:url" content="https://www.luhc.co.uk" />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="Lancaster University Hockey Club" />
-  <meta property="og:description" content="The official home page of Lancaster university hockey club. All are welcome." />
+  <meta property="og:title" content="Lancaster University Hockey Club | LUHC" />
+  <meta property="og:description" content="Join LUHC - the home of Lancaster Uni hockey. Fixtures, socials, photos, and everything LUHC." />
 
   <meta name="twitter:card" content="summary_large_image" />
   <meta property="twitter:domain" content="luhc.co.uk" />
   <meta property="twitter:url" content="https://www.luhc.co.uk" />
-  <meta name="twitter:title" content="Lancaster University Hockey Club" />
-  <meta name="twitter:description" content="The official home page of Lancaster university hockey club. All are welcome." />
+  <meta name="twitter:title" content="Lancaster University Hockey Club | LUHC" />
+  <meta name="twitter:description" content="Join LUHC - the home of Lancaster Uni hockey. Fixtures, socials, photos, and everything LUHC." />
+  
+  <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "SportsTeam",
+      "name": "Lancaster University Hockey Club",
+      "alternateName": "LUHC",
+      "sport": "Field Hockey",
+      "url": "https://www.luhc.co.uk",
+      "logo": "https://www.luhc.co.uk/favicon.png",
+      "memberOf": {
+        "@type": "CollegeOrUniversity",
+        "name": "Lancaster University",
+        "url": "https://www.lancaster.ac.uk",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Bailrigg",
+          "addressLocality": "Lancaster",
+          "addressRegion": "Lancashire",
+          "postalCode": "LA1 4YW",
+          "addressCountry": "GB"
+        }
+      },
+      "location": {
+        "@type": "Place",
+        "name": "Lancaster University Sports Centre",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Bigforth Drive, Bailrigg",
+          "addressLocality": "Lancaster",
+          "addressRegion": "Lancashire",
+          "postalCode": "LA1 4YW",
+          "addressCountry": "GB"
+        }
+      },
+      "sameAs": [
+        "https://www.facebook.com/groups/340322369353793/",
+        "https://www.instagram.com/luwhc_/",
+        "https://www.instagram.com/lumhc_/",
+        "https://www.instagram.com/luhc_dev/",
+        "https://x.com/theofficialuwhc",
+        "https://x.com/lumhc"
+      ]
+    }
+  </script>
 </svelte:head>
 
 <Header />
