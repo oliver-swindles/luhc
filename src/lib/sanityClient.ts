1b33a2565169cd15a38621fe790992614075b0e0
Oliver Swindles <86194326+oliver-swindles@users.noreply.github.com>
2025-06-01 23:29:29 +0100

    Added env

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@sanity/client";
+import {
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
+} from "$env/static/public";
 
 export const client = createClient({
-  projectId: "7hdfuzec", // Find this in your sanity.json or at manage.sanity.io
-  dataset: "production", // Or whichever dataset you're using
+  projectId: PUBLIC_SANITY_PROJECT_ID, // Find this in your sanity.json or at manage.sanity.io
+  dataset: PUBLIC_SANITY_DATASET, // Or whichever dataset you're using
   useCdn: false, // `false` if you want to ensure fresh data
   apiVersion: "2025-06-01", // Use a recent date
 });
