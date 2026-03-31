a3860f514024db016299c65796ad50cf1b165f38
Oliver Swindles <86194326+oliver-swindles@users.noreply.github.com>
2026-03-31 21:07:05 +0100

    Further security headers

---
+++ luhc-website/src/hooks.server.ts
@@ -0,0 +1,17 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }): Promise<Response> => {
+  const response: Response = await resolve(event);
+
+  response.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-XSS-Protection', '1; mode=block');
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  response.headers.set(
+    'Permissions-Policy',
+    'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()'
+  );
+
+  return response;
+};
